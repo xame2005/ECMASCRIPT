9800000b044d73baf65331864a7836531b27fe54
Samuel Gonzalez <xame2005@gmail.com>
2020-09-09 23:15:29 -0500

    Trabajando con funciones añadidas en ES7

---
+++ src/es7/index.js
@@ -0,0 +1,11 @@
+let numbers = [1, 2, 3, 7, 8];
+
+if (numbers.includes(4)) {
+  console.log("Sí se encuentra el valor 7");
+} else console.log("No se encuentra el valor");
+
+let base = 4;
+let exponent = 3;
+
+let result = base ** exponent;
+console.log(result);
